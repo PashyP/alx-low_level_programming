8d26468fae6f0409cbadad83e5cc006aeac661d6
PashyP <pash.shivuri@gmail.com>
2023-11-16 03:06:01 -0800

    freedo

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,23 @@
+; Declare needed C functions
+    extern  printf      ; the declared C function
+
+section .data       ; Data section with initialized variables
+msg:    db "Hello, Holberton", 0  ; C string
+fmt:    db "%s", 10, 0      ; Format specifier, "\n", '0'
+
+section .text       ; Code section.
+
+global main         ; the standard gcc entry point
+main:               ; the program label for the entry point
+    push    rbp     ; set up aligned stack frame
+
+    mov     rdi, fmt ; load format specifier address into rdi
+    mov     rsi, msg ; load message string address into rsi
+    mov     rax, 0   ; load format and message string address
+    call    printf  ; Call printf function
+
+    pop     rbp     ; restore stack
+
+    mov     rax, 0   ; return value (no error)
+    ret             ; return
+
